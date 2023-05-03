9212ad0d048e79b24d7f9e31aff3c1d5cac1fb73
Elif <gussayel@student.gu.se>
2023-05-03 11:44:31 +0200

    #21 feat: implement the user login UI and function
    When the user click on the button user in the first page or the main page of our app it takes the user to the login page where can the user put in its cardinalities in the right field and then click on the login button if the login been verified it provide the user to access  other part of the app. we have tow kind of user General and admin.
    
    Co-authored-By: Elif Yaqoub Gussayel@student.gu.se

--- app/src/main/java/com/intelligenttransportation/MainActivity.java
+++ app/src/main/java/com/intelligenttransportation/MainActivity.java
@@ -1,14 +1,54 @@
 package com.intelligenttransportation;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    Button userB,autoMobileConsoleB,trafficLightB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button userB= findViewById(R.id.userButton);
+        Button autoMobileConsoleB= findViewById(R.id.autoMoConButton);
+        Button trafficLightB= findViewById(R.id.trafficLightButton);
+
+
+        //Create the users objects
+        Controller.addUser("Jessei","1234","Admin");
+        Controller.addUser("Elif","1234", "Admin");
+        Controller.addUser("Tehreem","1234","General user");
+        Controller.addUser("YenigChao","1234","General user");
+        Controller.addUser("Hasson","1234", "General user");
+
+        userB.setOnClickListener(this);
+        autoMobileConsoleB.setOnClickListener(this);
+        trafficLightB.setOnClickListener(this);
+    }
+    @Override
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.userButton:
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.autoMoConButton:
+                Intent intent2 = new Intent(this, UserActivity.class);
+                startActivity(intent2);
+                break;
+            case R.id.trafficLightButton:
+                Intent intent3 = new Intent(this, TrafficLightActivity.class);
+                startActivity(intent3);
+                break;
+
+
+        }
     }
 }
