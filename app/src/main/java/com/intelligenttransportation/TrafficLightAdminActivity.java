329b1272a267b4330f7fc64c90dc111dd4d5cdcd
jessie <luhui930@126.com>
2023-05-06 21:48:20 +0200

    #18 feat:Implement Publishing of MQTT connection in Android
    add button'button_west_east'function publish data to wio terminal to complete Admin can control of the traffic light
    
    Co-Authored-By:Jessie Johnsson <gusjohjefh@student.gu.se>

--- app/src/main/java/com/intelligenttransportation/TrafficLightAdminActivity.java
+++ app/src/main/java/com/intelligenttransportation/TrafficLightAdminActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -18,7 +19,7 @@ public class TrafficLightAdminActivity extends AppCompatActivity {
     public static final String LOCALHOST = "broker.emqx.io";
     private static final String MQTT_SERVER = "tcp://" + LOCALHOST + ":1883";
     public static final String CLIENT_ID = "Android App";
-    public static final int QOS = 0;
+    public static final int QOS = 2;
     private BottomNavigationView bottomNavigationView;
 
     @Override
@@ -27,6 +28,7 @@ public class TrafficLightAdminActivity extends AppCompatActivity {
         setContentView(R.layout.activity_traffic_light_admin);
 
         broker = new BrokerConnection(getApplicationContext());
+        mqttClient = broker.getMqttClient();
         broker.setConnectionMessage(findViewById(R.id.textView_west_east_number),
                 findViewById(R.id.textView_north_south_number),
                 findViewById(R.id.light_east),
@@ -37,8 +39,13 @@ public class TrafficLightAdminActivity extends AppCompatActivity {
                 findViewById(R.id.light_north_south));
         broker.connectToMqttBroker();
 
-
-
+        Button eastGo = findViewById(R.id.button_west_east);
+        eastGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mqttClient.publish(PUB_TOPIC, DisplayUtils.eastGoAdmin, QOS, null);
+            }
+        });
 
         bottomNavigationView = findViewById(R.id.bottom_navigation_menu);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
