091f06a364b75d10bfbfe64c064ef7be564dfd2b
Yingchao Ji <gusjiyid@student.gu.se>
2023-05-06 20:31:56 +0200

    #26 feat: Add north car
     Add north car on north road. Add image_car_north ImageView in both activity_traffic_light_user.xml and activity_traffic_light_admin.xml. If PIR sensor detect the car, then show the image.
    
     Co-Authored-By: Yingchao Ji  gusjiyid@student.gu.se

--- app/src/main/java/com/intelligenttransportation/TrafficLightUserActivity.java
+++ app/src/main/java/com/intelligenttransportation/TrafficLightUserActivity.java
@@ -7,8 +7,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -27,7 +25,7 @@ public class TrafficLightUserActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_triffic_light_user);
+        setContentView(R.layout.activity_traffic_light_user);
 
         broker = new BrokerConnection(getApplicationContext());
         mqttClient = broker.getMqttClient();
