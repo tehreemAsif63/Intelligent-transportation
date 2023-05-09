b85d8fb9dcb27ba2ceb9c8e65319954c04939e3f
jessie <luhui930@126.com>
2023-05-09 13:38:58 +0200

    #11 feat:Implement Account Information page
    Implement Account Information page buttons jump into the bind car page
    
    Co-Authored-By:Jessie Johnsson <gusjohjefh@student.gu.se>

--- app/src/main/java/com/intelligenttransportation/AccountInformation.java
+++ app/src/main/java/com/intelligenttransportation/AccountInformation.java
@@ -2,7 +2,10 @@ package com.intelligenttransportation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AccountInformation extends AppCompatActivity {
 
@@ -10,5 +13,37 @@ public class AccountInformation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_information);
+
+        Button buttonBack = findViewById(R.id.button_back);
+        Button buttonBind = findViewById(R.id.button_bind);
+        Button buttonLogout = findViewById(R.id.button_logout);
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+
+        buttonBind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AccountInformation.this,bindCar.class);
+                startActivity(intent);
+            }
+        });
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Intent intent = new Intent(user_infor.this,bindCar1.class);
+                startActivity(intent);*/
+
+            }
+        });
+
+
+
+
+
     }
 }
