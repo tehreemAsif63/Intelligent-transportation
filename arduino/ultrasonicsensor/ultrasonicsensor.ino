6d55519353474d72370b5fa9d54f47ab55c0b16e
Elif <yaqoub@chalmers.se>
2023-05-15 11:09:44 +0200

    #7 feat:Implemanted ultrasonic function for east side. Added the code block to detect cars passing by a sensor in the east direction, and then send a message to a server to record the count of cars passing by.flag is numbers that represents the status of traffic lights.if(flag >= 20 && flag < 37){  //This is mean east traffic light is red, so we start count number of cars. The (ultraEast) variable is an object that represents an ultrasonic sensor that is used to measure the distance to an object. In this case, it is being used to detect when a car passes by the sensor.If the distance measured by the sensor is less than 10 centimeters, then a car is considered to have passed by. The (isNewCarEast) variable is used to ensure that each car is only counted once, to avoid duplicate counting. If a new car is detected, the (tone()) function is used to play a sound on a buzzer to indicate that a car has passed by. The (countEastCar) variable is incremented to keep track of the number of cars that have passed by from the east direction.If a connection to a server is established (client.connect(ID)), then a message is sent to the server using the MQTT protocol. The message includes the count of eastbound cars and the count of northbound cars (which is currently set to 0).Finally, a message is displayed on a screen using the (spr.drawString()) function to indicate that a car has passed by from the east direction. Co-Authored-By: Elif Yaqooub  gussayel@student.gu.se

--- arduino/ultrasonicsensor/ultrasonicsensor.ino
+++ arduino/ultrasonicsensor/ultrasonicsensor.ino
@@ -18,7 +18,24 @@ carOnEast();
 void carOnEast(){
   if(flag >= 20 && flag < 37){
     double distance = ultraEast.MeasureInCentimeters();
+    
     if(distance < 10){
+       if(!isNewCarEast){ //To avoid duplicate counting
+          isNewCarEast = true;
+          tone(WIO_BUZZER, 100, 1000);
+          countEastCar ++;
+          if (client.connect(ID)) {
+            String data = "east:car:" + String(countEastCar) + ";north:car:0";
+            client.publish(TOPIC, data.c_str());
+          }
+          flag += 4;
+          if(flag > 37){
+            flag = 37;
+          }
+      }
+      spr.drawString("Car Come from the east.",20,100);
+      //Serial.println("Car Come from the east.");
+
     }
     else{
       isNewCarEast = false;
