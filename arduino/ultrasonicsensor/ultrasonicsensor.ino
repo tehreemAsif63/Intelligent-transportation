a82f823e951d49471a53d08f4a4d060472865090
Elif <yaqoub@chalmers.se>
2023-05-15 11:48:03 +0200

    #7 feat:Implemanted ultrasonic function for North side.Added code to detect and count cars coming from the north using an ultrasonic sensor. Data is sent to a MQTT broker for further processing. Also added logic to avoid duplicate counting of cars.Furthermore, a tone is played when a new car is detected and displays a message indicating the direction of the car. The code also ensures that the flag variable stays within a certain range to prevent errors. Overall, this code provides an efficient and accurate way of detecting and counting cars coming from the north.The same way of east side. Co-Authored-By: Elif Yaqooub  gussayel@student.gu.se

--- arduino/ultrasonicsensor/ultrasonicsensor.ino
+++ arduino/ultrasonicsensor/ultrasonicsensor.ino
@@ -12,6 +12,7 @@ void setup() {
 void loop() {
 
 carOnEast();
+carOnNorth();
 
 }
 
@@ -35,10 +36,35 @@ void carOnEast(){
       }
       spr.drawString("Car Come from the east.",20,100);
       //Serial.println("Car Come from the east.");
-
     }
     else{
       isNewCarEast = false;
+    }
+  }
+}
+
+void carOnNorth(){
+  if(flag >= 0 && flag < 17){
+    double distance = 0.01723 * readUltrasonicDuration(ultraNorth, ultraNorth);
+    if(distance < 10){ 
+      if(!isNewCarNorth){//To avoid duplicate counting
+        isNewCarNorth = true;
+        tone(WIO_BUZZER, 100, 1000);
+        countNorthCar ++;
+        if (client.connect(ID)) {
+          String data = "east:car:0;north:car:" + String(countNorthCar);
+          client.publish(TOPIC, data.c_str());
+        }
+        spr.drawString("Car Come from the north.",20,100);
+        flag += 5;
+        if(flag > 17 && flag < 37){
+          flag = 17;
+        }
+      }
+      spr.drawString("Car Come from the north.",20,100);
+      //Serial.println("Car Come from the north.");
+    }else{
+      isNewCarNorth = false;
     }
   }
 }
