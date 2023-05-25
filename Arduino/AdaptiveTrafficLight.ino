58f743c7392f4f333db2673043c21169c6acec6a
Yingchao Ji <gusjiyid@student.gu.se>
2023-05-25 20:34:39 +0200

    #5 comment: Add comment for code
    Add comment for AdaptiveTrafficLight code. Because the code is not very easy to understand. Adding comments makes the code easier to understand
    
    Co-Authored-By: Yingchao Ji    gusjiyid@student.gu.se

--- Arduino/AdaptiveTrafficLight.ino
+++ Arduino/AdaptiveTrafficLight.ino
@@ -3,15 +3,16 @@
 #include <PubSubClient.h>
 
 
-// Update these with values suitable for your network.
+// update these with values suitable for your network.
 const char *ssid = "SSID";      // your network SSID
 const char *password = "password"; // your network password
 
-const char *ID = "Wio-Terminal-Client";  // Name of our device, must be unique
-const char *TOPIC = "group9_outTopic";  // Topic to subcribe to
-const char *subTopic = "group9_inTopic";  // Topic to subcribe to
-const char *server = "broker.emqx.io"; // Server URL
+const char *ID = "Wio-Terminal-Client";  // name of our device, must be unique
+const char *TOPIC = "group9_outTopic";  // topic to subcribe to
+const char *subTopic = "group9_inTopic";  // topic to subcribe to
+const char *server = "broker.emqx.io"; // server URL
 
+//wifi and MQTT connection client
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
 //print on Wio terminal
@@ -19,8 +20,8 @@ TFT_eSPI tft;
 TFT_eSprite spr = TFT_eSprite(&tft);
 
 
-// Defining the pins in Wio terminal
-// Defining the pins for the LED lights
+// defining the pins in Wio terminal
+// defining the pins for the LED lights
 const int redEast = 2;
 const int yellowEast = 3;
 const int greenEast = 4;
@@ -29,32 +30,32 @@ const int redNorth = 5;
 const int yellowNorth = 6;
 const int greenNorth = 7;
 
-// Defining the pins for the ultrasonic sensors
+// defining the pins for the ultrasonic sensors
 int ultraEast = 1;
 int ultraNorth = 8;
 
-int maxFlag = 40;
-int eastYellowPoint = maxFlag / 2 - 3;
-int halfMaxFlag = maxFlag / 2;
-int northYellowPoint = maxFlag - 3;
-int flag = 0;
-int tempFlag = -1;
-int countEastCar = 0;
-int countNorthCar = 0;
-bool isNewCarEast = false; //To avoid duplicate counting
-bool isNewCarNorth = false; //To avoid duplicate counting
+int maxFlag = 40; // define the max number of flag, maxFlag / 2 is the time that the red light lasts in each direction
+int eastYellowPoint = maxFlag / 2 - 3; // yellow light on east is on in this number
+int halfMaxFlag = maxFlag / 2; // red light on east is on in this number
+int northYellowPoint = maxFlag - 3; // yellow light on north is on in this number
+int flag = 0; // flag number represents a traffic light state
 
-int userType = 0; //0 is no case; 1 is general user; 2 is admin user
-int whichGo = 0; // 0 is no case; 1 is east-west; 2 is north-south
-bool isRenew = false; // true is renew to original state
-bool isPause = false; // true is immediately pause the traffic light status on both sides
+int countEastCar = 0; // count the car passing east side
+int countNorthCar = 0; // count the car passing north side
+bool isNewCarEast = false; // determine if there is double counting on east, to avoid duplicate counting
+bool isNewCarNorth = false; // determine if there is double counting on east, to avoid duplicate counting
 
-unsigned long previousMillis_traffic = 0;
-const long interval_traffic = 1000;
+int userType = 0; // the type of user: 0 is no case; 1 is general user; 2 is admin user
+int whichGo = 0; // control which side: 0 is no case; 1 is east-west; 2 is north-south
+bool isPause = false; // whether to pause traffic lights: true is immediately pause the traffic light status on both sides
+bool isRenew = false; // whether to restore traffic lights: true is restore traffic lights
+
+unsigned long previousMillis_traffic = 0; //previous time
+const long interval_traffic = 1000; //interval time is 1 second
 
 void setup() {
-  WiFi.begin(ssid, password);
-  // attempt to connect to Wifi network:
+  WiFi.begin(ssid, password); //set wi-fi username and password to connect wi-fi
+  // attempt to connect to Wi-fi network:
   while (WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     WiFi.begin(ssid, password);
@@ -77,6 +78,7 @@ void setup() {
 
   Serial.begin(9600);
 
+  // draw on the Wio terminal
   tft.begin();
   tft.setRotation(3);
   spr.createSprite(TFT_HEIGHT,TFT_WIDTH);
@@ -86,67 +88,68 @@ void setup() {
 void loop() {
 
   client.loop();
-  client.subscribe(subTopic);
+  client.subscribe(subTopic); // subscribe message, and process the data in the callback function
 
-  unsigned long currentMillis_traffic = millis();
-  if (currentMillis_traffic - previousMillis_traffic >= interval_traffic) {
+  unsigned long currentMillis_traffic = millis(); // get the current time
+  if (currentMillis_traffic - previousMillis_traffic >= interval_traffic) { // if the time has passed interval time(1 second)
 
-    if(flag >= maxFlag){
+    if(flag >= maxFlag){ // if flag >= maxFlag, let it = 0 to loop
         flag = 0;
     }
 
-    carOnEast();
-    carOnNorth();
+    carOnEast(); // count the number of vehicles in the east
+    carOnNorth(); // count the number of vehicles in the north
 
-    userControl();
-    adminControl();
+    userControl(); // general user control
+    adminControl(); // admin user control
 
-    trafficLight();
+    trafficLight(); // normal traffic light system
 
-    if(!isPause){
-        flag++;
+    if(!isPause){ // if traffic light is not be paused. if isPause = true, flag should not ++
+        flag ++; // flag number represents a traffic light state. flag ++ mean traffic light state --, time remaining of traffic lights --
     }
 
     printTitleOnWioTerminal();
-    spr.pushSprite(0,0);//print on Wio terminal
+    spr.pushSprite(0,0);//print on Wio terminal, start drawing at x0, y0
 
-    previousMillis_traffic = currentMillis_traffic;
+    previousMillis_traffic = currentMillis_traffic; // let previous time = current time, so we can calculate the time for the next second
   }
 
 }
 
-void trafficLight(){
-    if(flag < eastYellowPoint){
+void trafficLight(){ // normal traffic light system
+    if(flag < eastYellowPoint){ //before yellow light is green light on east
         greenOnEast();
     }
-    if(flag >= eastYellowPoint && flag < halfMaxFlag){
+    if(flag >= eastYellowPoint && flag < halfMaxFlag){ // between the green and red lights is a yellow light on east
         yellowOnEast();
     }
-    if(flag >= halfMaxFlag && flag < northYellowPoint){
+    if(flag >= halfMaxFlag && flag < northYellowPoint){ //before yellow light is green light on north
         greenOnNorth();
     }
-    if(flag >= northYellowPoint && flag < maxFlag){
+    if(flag >= northYellowPoint && flag < maxFlag){ // between the green and red lights is a yellow light on north
         yellowOnNorth();
     }
 }
 
 void greenOnEast(){
-
+      // Set the status of each light on east
       digitalWrite(redEast, LOW);
       digitalWrite(yellowEast, LOW);
       digitalWrite(greenEast, HIGH);
-
+      // Set the status of each light on north
       digitalWrite(redNorth, HIGH);
       digitalWrite(yellowNorth, LOW);
       digitalWrite(greenNorth, LOW);
 
-      countEastCar = 0;
+      countEastCar = 0; // When the east side is green light, vehicles pass, countEastCar = 0
 
-      if (client.connect(ID)) {
+      if (client.connect(ID)) { // if the connection is successful
+        // The format of message is:   east:color:time;north:color:time
         String data = "east:green:" + String(eastYellowPoint - flag) + ";north:red:" + String(halfMaxFlag - flag);
-        client.publish(TOPIC, data.c_str());
+        client.publish(TOPIC, data.c_str()); // publish message
       }
-
+      // draw the information in Wio terminal
       spr.setTextColor(TFT_GREEN);
       spr.drawString("East:  Green light: ",20,50);
       spr.drawNumber(eastYellowPoint - flag,260,50);
@@ -226,35 +229,36 @@ void yellowOnNorth(){
     spr.drawNumber(maxFlag - flag,260,75);
 }
 
-void carOnEast(){
-	if(flag >= halfMaxFlag && flag < northYellowPoint){
-    double distance = 0.01723 * readUltrasonicDuration(ultraEast, ultraEast);
-  	if(distance < 10){ 
-      if(!isNewCarEast){ //To avoid duplicate counting
-          isNewCarEast = true;
-          tone(WIO_BUZZER, 100, 1000);
-          countEastCar ++;
-          if (client.connect(ID)) {
+void carOnEast(){ // count the number of vehicles on east
+	if(flag >= halfMaxFlag && flag < northYellowPoint){ // red light on the east side
+    double distance = 0.01723 * readUltrasonicDuration(ultraEast, ultraEast); // calculate distance. 0.01723 = sonic_speed / 2 / 10000
+  	if(distance < 10){ //when distance < 10, we think there was a car passing
+      if(!isNewCarEast){ //to avoid duplicate counting. Only newly passed vehicles will be counted
+          isNewCarEast = true; // set isNewCarEast true, if it is the same car, it will not be counted in the next second
+          tone(WIO_BUZZER, 100, 1000); // Tone of the prompt
+          countEastCar ++; // number of car ++
+          if (client.connect(ID)) { // if the connection is successful
+            // The format of message is:   east:car:number;north:car:number
             String data = "east:car:" + String(countEastCar) + ";north:car:0";
-            client.publish(TOPIC, data.c_str());
+            client.publish(TOPIC, data.c_str()); // publish message
           }
-          flag += 4;
-          if(flag > northYellowPoint){
-          	flag = northYellowPoint;
-          }
+          flag += 4; // flag + 4 mean The remaining time of the traffic light minus 4 seconds
+          if(flag > northYellowPoint){ // if the flag exceeds the start time of the yellow light on north
+          	flag = northYellowPoint; // it should be returned to the start time of the yellow light.
+          } // because it is necessary to ensure that there are three seconds of yellow light between the green light and the red light.
       }
-      spr.drawString("Car Come from the east.",20,100);
-    }else{
-    	isNewCarEast = false;
+      spr.drawString("Car Come from the east.",20,100); // draw message in the Wio terminal
+    }else{ // if distance >= 10, it means that the vehicle has passed or there is no vehicle
+    	isNewCarEast = false; // set isNewCarEast = false, so that we can continue to counting vehicles
     }
   }
 }
 
-void carOnNorth(){
+void carOnNorth(){ // count the number of vehicles on north
 	if(flag >= 0 && flag < eastYellowPoint){
     double distance = 0.01723 * readUltrasonicDuration(ultraNorth, ultraNorth);
-  	if(distance < 10){ //To avoid duplicate counting
-      if(!isNewCarNorth){
+  	if(distance < 10){
+      if(!isNewCarNorth){ //To avoid duplicate counting
         isNewCarNorth = true;
         tone(WIO_BUZZER, 100, 1000);
         countNorthCar ++;
@@ -275,63 +279,68 @@ void carOnNorth(){
   }
 }
 
-void userControl(){
-  if(userType == 1){
-      flag += 3;
-      userType = 0;
-      if(flag > eastYellowPoint && flag < halfMaxFlag){
-        flag = eastYellowPoint;
-      }
-      if(flag > northYellowPoint){
-        flag = northYellowPoint;
+void userControl(){ // general user control
+  if(userType == 1){ // userType == 1 mean user is general user
+      flag += 3; // flag + 3 mean The remaining time of the traffic light minus 3 seconds
+      userType = 0; // after control, let userType = 0, ensure it is executed only once
+      if(flag > eastYellowPoint && flag < halfMaxFlag){ // if the flag exceeds the start time of the yellow light on east
+        flag = eastYellowPoint; // it should be returned to the start time of the yellow light.
+      } // because it is necessary to ensure that there are three seconds of yellow light between the green light and the red light.
+      if(flag > northYellowPoint){ // if the flag exceeds the start time of the yellow light on north
+        flag = northYellowPoint; // it should be returned to the start time of the yellow light.
       }
   }
 }
-void adminControl(){
-  if(userType == 2){
-    if(isRenew){
-      isPause = false;
-      isRenew = false;
+void adminControl(){ // admin user control
+  if(userType == 2){ // userType == 2 mean user is admin user
+    if(isRenew){ // renew is true means restoring traffic lights
+      isPause = false; // flag can continue ++. Check the flag ++ section in loop() method
+      isRenew = false; // ensure it is executed only once
     }
-    if(whichGo == 1){
-      flag = northYellowPoint;
-      whichGo = 0;
-    }
-
-    if(whichGo == 2){
-      flag = eastYellowPoint;
-      whichGo = 0;
+    if(whichGo == 1){ // it means to let the east side pass
+      flag = northYellowPoint; // make sure there is a 3-second yellow light between the green light and the red light in the north to ensure safety
+      whichGo = 0; // ensure it is executed only once
     }
 
+    if(whichGo == 2){ // it means to let the north side pass
+      flag = eastYellowPoint; // make sure there is a 3-second yellow light between the green light and the red light in the east to ensure safety
+      whichGo = 0; // ensure it is executed only once
+    }
+
+    //pause control is in loop() method
   }
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char* topic, byte* payload, unsigned int length) { // subscribe message from MQTT
   for (int i=0;i<length;i++) {
-    Serial.print((char)payload[i]);
+    Serial.print((char)payload[i]); // print information in console
   }
   Serial.println();
-  
-  if(payload[0]-48 == 1){ //payload[0]-48 is to make byte to int
-    userType = payload[0]-48;
-    userControl();
+  // payload[0] stands for userType
+  // payload[1] stands for whichGo
+  // payload[2] stands for isRenew
+  // payload[3] stands for isPause
+
+  if(payload[0]-48 == 1){ // payload[0]-48 is to make the type to int
+    userType = payload[0]-48; // set user type to general user
+    userControl(); // execute general user control
   }
   if(payload[0]-48 == 2){
-    userType = 2;
-    whichGo = payload[1] - 48;
+    userType = 2; // set user type to admin user
+    whichGo = payload[1] - 48; // set whichGo
     
-    if(payload[2]-48 == 1){
+    if(payload[2]-48 == 1){ // payload[2]-48 == 1 mean isRenew = true
       isRenew = true;
     }
-    if(payload[3]-48 == 1){
+    if(payload[3]-48 == 1){ // payload[3]-48 == 1 mean isPause = true
       isPause = true;
     }
-    adminControl();
+    adminControl(); // execute general user control
   }
   
 }
 
-long readUltrasonicDuration(int triggerPin, int echoPin){
+long readUltrasonicDuration(int triggerPin, int echoPin){ // calculate the time it takes for a sound wave to go from emitting to returning
   pinMode(triggerPin, OUTPUT);  // Clear the trigger
   digitalWrite(triggerPin, LOW);
   delayMicroseconds(2);
@@ -344,7 +353,7 @@ long readUltrasonicDuration(int triggerPin, int echoPin){
   return pulseIn(echoPin, HIGH);
 }
 
-void printTitleOnWioTerminal() {
+void printTitleOnWioTerminal() { // print title on wio terminal
   
   spr.fillSprite(TFT_BLACK);
   spr.setTextSize(2);
